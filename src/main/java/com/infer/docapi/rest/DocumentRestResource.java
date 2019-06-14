ceab6f6cdfaa6ceb4233f7ca38930c3267504bba
teekmj <teekmj@gmail.com>
2019-06-14 22:07:41 +0530

    Allowing cross

--- src/main/java/com/infer/docapi/rest/DocumentRestResource.java
+++ src/main/java/com/infer/docapi/rest/DocumentRestResource.java
@@ -3,22 +3,26 @@ package com.infer.docapi.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infer.docapi.domain.PolicyDoc;
 import com.infer.docapi.service.DocService;
 
+
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/documents")
 public class DocumentRestResource {
 	
 	@Autowired
 	DocService docService;
 	
-	@GetMapping(path = "/read",  produces = "application/json")
+	@PostMapping(path = "/read",  produces = "application/json")
 	public List<PolicyDoc> readDocuments() {
 		return docService.readAllDocs();
 	}
