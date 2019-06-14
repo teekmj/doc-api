4fedd9e84f5b9ae3dbf6a46a455b9bcd8675f76c
teekmj <teekmj@gmail.com>
2019-06-14 22:17:49 +0530

    cros

--- src/main/java/com/infer/docapi/rest/DocumentRestResource.java
+++ src/main/java/com/infer/docapi/rest/DocumentRestResource.java
@@ -3,6 +3,7 @@ package com.infer.docapi.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.infer.docapi.service.DocService;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/documents")
 public class DocumentRestResource {
 	
