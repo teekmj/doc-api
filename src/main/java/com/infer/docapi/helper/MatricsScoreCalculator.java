52702acf0f8137232a3f6683f9860cd00c7d3fde
teekmj <teekmj@gmail.com>
2019-06-15 17:23:18 +0530

    adding comments

--- src/main/java/com/infer/docapi/helper/MatricsScoreCalculator.java
+++ src/main/java/com/infer/docapi/helper/MatricsScoreCalculator.java
@@ -10,6 +10,12 @@ public class MatricsScoreCalculator {
 	private static final double COPY_COUNT = 0.7;
 	private static final double MOUSE_OVER_COUNT = 0.3;
 	
+	/**
+	 * Calculate the score of a paragraph using the event data
+	 * more events to be added and factors to be refined
+	 * @param interactionMatrics
+	 * @return
+	 */
 	public Double calculateScore(UserInteractionMatrics interactionMatrics) {
 		return (interactionMatrics.getCopyCount() * COPY_COUNT + interactionMatrics.getMouseOverCount() * MOUSE_OVER_COUNT) ;
 	}
